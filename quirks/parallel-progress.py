14c582c92b455de4ec017812add6c81f5bfbcd60
Subhrajyoty Roy <subhrajyotyroy@gmail.com>
2024-02-27 20:04:49 +0530

    Create parallel-progress.py

---
+++ quirks/parallel-progress.py
@@ -0,0 +1,24 @@
+import multiprocessing as mp
+import tqdm
+from time import sleep
+
+cpu_count = mp.cpu_count() - 1
+tasks = range(35)
+
+def do_work(task):
+    sleep(1)
+    return task
+
+# THIS IS IMPORTANT
+# In windows, all the subprocesses import the calling scripts for the dependencies
+# adding name = main stops subprocesses to recursively generate further subprocesses
+# so, the time.sleep function is present
+if __name__ == "__main__":
+    try:
+        with mp.Pool(processes=cpu_count) as pool:
+            mapped_values = list(tqdm.tqdm(pool.imap_unordered(do_work, tasks), total=len(tasks)))
+            pool.close()
+
+        print(mapped_values)
+    except Exception as e:
+        print(e)
